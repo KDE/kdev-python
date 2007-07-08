cba211d5eb8e6dc19e4f9c315978ce4a140aae71
Piyush Verma <piyush.verma@gmail.com>
2007-07-08 20:25:17 +0000

    Added the pythoastvisitor header. To be Implemented to Override the default import visit functions.
    svn path=/trunk/playground/devtools/kdevelop4-extra-plugins/python/; revision=685417

---
+++ parser/pythonastvisitor.h
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ * Copyright (c) 2007 Andreas Pakulat <apaku@gmx.de>                         *
+ * Copyright (c) 2007 Piyush verma <piyush.verma@gmail.com>                  *
+ *                                                                           *
+ * Permission is hereby granted, free of charge, to any person obtaining     *
+ * a copy of this software and associated documentation files (the           *
+ * "Software"), to deal in the Software without restriction, including       *
+ * without limitation the rights to use, copy, modify, merge, publish,       *
+ * distribute, sublicense, and/or sell copies of the Software, and to        *
+ * permit persons to whom the Software is furnished to do so, subject to     *
+ * the following conditions:                                                 *
+ *                                                                           *
+ * The above copyright notice and this permission notice shall be            *
+ * included in all copies or substantial portions of the Software.           *
+ *                                                                           *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,           *
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF        *
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                     *
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE    *
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION    *
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION     *
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.           *
+ *****************************************************************************/
+
+#ifndef PYTHON_AST_VISITOR
+#define PYTHON_AST_VISITOR
+
+#include "python_default_visitor.h"
+
+using namespace python;
+class PythonVisitor : public default_visitor
+{
+public:
+    virtual void visit_import_as_name(import_as_name_ast *node);
+    virtual void visit_import_as_names(import_as_names_ast *node);
+    virtual void visit_import_from(import_from_ast *node);
+    virtual void visit_import_name(import_name_ast *node);
+    virtual void visit_import_stmt(import_stmt_ast *node);
+};
+
+#endif

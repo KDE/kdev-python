84d6ad830e93a2c5de3b6e0da922f82df933ef93
Sven Brauch <svenbrauch@googlemail.com>
2010-10-17 15:28:27 +0200

    Hopefully the hand-crafted AST is now complete

--- parser/ast.cpp
+++ parser/ast.cpp
@@ -32,272 +32,272 @@ Ast::~Ast() { }
 
 ArgumentsAst::ArgumentsAst(Ast* parent, Ast::AstType type): Ast(parent, Ast::ArgumentsAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 AssertionAst::AssertionAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::AssertionAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 AssignmentAst::AssignmentAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::AssignmentAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 AttributeAst::AttributeAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::AttributeAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 AugmentedAssignmentAst::AugmentedAssignmentAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::AugmentedAssignmentAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 BinaryOperationAst::BinaryOperationAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::BinaryOperationAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 BooleanOperationAst::BooleanOperationAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::BooleanOperationAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 BreakAst::BreakAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::BreakAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 CallAst::CallAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::CallAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ClassDefinitionAst::ClassDefinitionAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::ClassDefinitionAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 CodeAst::CodeAst(Ast* parent, Ast::AstType type): Ast(parent, Ast::CodeAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 CompareAst::CompareAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::CompareAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ComprehensionAst::ComprehensionAst(Ast* parent, Ast::AstType type): Ast(parent, Ast::ComprehensionAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ContinueAst::ContinueAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::ContinueAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 DeleteAst::DeleteAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::DeleteAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 DictionaryComprehensionAst::DictionaryComprehensionAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::DictionaryComprehensionAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 EllipsisAst::EllipsisAst(Ast* parent, Ast::AstType type): SliceAstBase(parent, Ast::EllipsisAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ExceptionHandlerAst::ExceptionHandlerAst(Ast* parent, Ast::AstType type): Ast(parent, Ast::ExceptionHandlerAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ExecAst::ExecAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::ExecAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ExpressionAst::ExpressionAst(Ast* parent, Ast::AstType type): Ast(parent, Ast::ExpressionAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ExtendedSliceAst::ExtendedSliceAst(Ast* parent, Ast::AstType type): SliceAstBase(parent, Ast::ExtendedSliceAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ForAst::ForAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::ForAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 FunctionDefinitionAst::FunctionDefinitionAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::FunctionDefinitionAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 GeneratorExpressionAst::GeneratorExpressionAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::GeneratorExpressionAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 GlobalAst::GlobalAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::GlobalAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 Identifier::Identifier(QString value) : value(value)
 {
-
+    
 }
 
 IfAst::IfAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::IfAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 IfExpressionAst::IfExpressionAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::IfExpressionAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ImportAst::ImportAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::ImportAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ImportFromAst::ImportFromAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::ImportFromAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 KeywordAst::KeywordAst(Ast* parent, Ast::AstType type): Ast(parent, Ast::KeywordAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 LambdaAst::LambdaAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::LambdaAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ListAst::ListAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::ListAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 NameAst::NameAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::NameAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 NumberAst::NumberAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::NumberAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 PassAst::PassAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::PassAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 PrintAst::PrintAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::PrintAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 RaiseAst::RaiseAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::RaiseAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ReprAst::ReprAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::ReprAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 ReturnAst::ReturnAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::ReturnAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 SetAst::SetAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::SetAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 SetComprehensionAst::SetComprehensionAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::SetComprehensionAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 SliceAstBase::SliceAstBase(Ast* parent, Ast::AstType type): Ast(parent, Ast::SliceAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 StatementAst::StatementAst(Ast* parent, Ast::AstType type): Ast(parent, Ast::StatementAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 StringAst::StringAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::StringAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 SubscriptAst::SubscriptAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::SubscriptAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 TryExceptAst::TryExceptAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::TryExceptAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 TryFinallyAst::TryFinallyAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::TryFinallyAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 TupleAst::TupleAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::TupleAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 UnaryOperationAst::UnaryOperationAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::UnaryOperationAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 WhileAst::WhileAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::WhileAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 WithAst::WithAst(Ast* parent, Ast::AstType type): StatementAst(parent, Ast::WithAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 YieldAst::YieldAst(Ast* parent, Ast::AstType type): ExpressionAst(parent, Ast::YieldAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 AliasAst::AliasAst(Ast* parent, Ast::AstType type): Ast(parent, Ast::AliasAstType)
 {
-
+    Q_UNUSED(type);
 }
 
 

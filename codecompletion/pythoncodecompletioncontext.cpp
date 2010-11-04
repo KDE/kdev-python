3e622041c67d57ebc22814ab6581c259a4a29b8d
Sven Brauch <svenbrauch@googlemail.com>
2010-11-04 01:52:35 +0100

    Screwy, but working (!) import file completion

--- codecompletion/pythoncodecompletioncontext.cpp
+++ codecompletion/pythoncodecompletioncontext.cpp
@@ -27,10 +27,16 @@ QList<CompletionTreeItemPointer> PythonCodeCompletionContext::completionItems(bo
 {
     QList<CompletionTreeItemPointer> items;
     
-    
     if ( m_operation == PythonCodeCompletionContext::ImportFileCompletion ) {
         m_maxFolderScanDepth = 1;
         foreach ( ImportFileItem* item, includeFileItems() ) {
+            item->includeItem.name = QString(item->moduleName + " (from " + KUrl::relativeUrl(item->fromProject->folder(), item->includeItem.basePath) + ")");
+            items << CompletionTreeItemPointer( item );
+        }
+    }
+    else if ( m_operation == PythonCodeCompletionContext::ImportSubCompletion ) {
+        kDebug() << "Stuff found for completion: " << findFilesForName(m_subForModule);
+        foreach ( ImportFileItem* item, findFilesForName(m_subForModule) ) {
             item->includeItem.name = QString(item->moduleName + " (from " + KUrl::relativeUrl(item->fromProject->folder(), item->includeItem.basePath) + ")");
             items << CompletionTreeItemPointer( item );
         }
@@ -47,7 +53,19 @@ QList<CompletionTreeItemPointer> PythonCodeCompletionContext::completionItems(bo
         }
     }
     
+    m_searchingForModule.clear();
+    m_subForModule.clear();
+    
     return items;
+}
+
+QList< ImportFileItem* > PythonCodeCompletionContext::findFilesForName(const QString& name)
+{
+    kDebug() << "Name: " << name;
+    QStringList resolvedName = name.split(".");
+    m_maxFolderScanDepth = resolvedName.length() + 1;
+    m_searchingForModule = resolvedName;
+    return includeFileItems();
 }
 
 QList<ImportFileItem*> PythonCodeCompletionContext::includeFileItems() {
@@ -65,13 +83,34 @@ QList<ImportFileItem*> PythonCodeCompletionContext::includeFileItems() {
 QList<ImportFileItem*> PythonCodeCompletionContext::fileItemsForFolder(KDevelop::ProjectFolderItem* folder, IProject* project)
 {
     if ( ! folder ) return QList<ImportFileItem*>();
+    
+    kDebug() << m_maxFolderScanDepth << m_folderStack.count() << m_searchingForModule;
+    
     if ( m_maxFolderScanDepth < m_folderStack.count() ) return QList<ImportFileItem*>();
+    
+    if ( m_searchingForModule.length() > 0 && folder->url() != project->folder().url() ) {
+        if ( m_searchingForModule.length() >= m_folderStack.count() && m_searchingForModule.at(m_folderStack.count() - 2) != folder->folderName() ) {
+            kDebug() << "Skip: " << m_searchingForModule.at(m_folderStack.count() - 2) << m_searchingForModule << m_folderStack.count() - 2 << folder->folderName();
+            return QList<ImportFileItem*>();
+        }
+        kDebug() << "USE: " << m_searchingForModule.at(m_folderStack.count() - 2) << m_searchingForModule << m_folderStack << folder->folderName();
+    }
+    
     QList<ImportFileItem*> items;
     foreach ( KDevelop::ProjectFolderItem* folder, folder->folderList() ) {
         if ( ! folder ) continue;
         m_folderStack.push(folder);
         kDebug() << "Scanning for include items: " << folder->folderName();
         items << fileItemsForFolder(folder, project);
+        
+        // only add items when at right level
+        if ( m_searchingForModule.length() != 0 && m_maxFolderScanDepth != m_folderStack.count() ) {
+            kDebug() << "CONTINUE: " << m_maxFolderScanDepth << m_folderStack.count();
+            m_folderStack.pop();
+            continue;
+        }
+        kDebug() << "ADD: " << m_maxFolderScanDepth << m_folderStack.count();
+        kDebug() << "adding files and folders from directory " << folder->folderName();
         
         // Add the folder
         IncludeItem* folderItem = new IncludeItem();
@@ -100,11 +139,31 @@ QList<ImportFileItem*> PythonCodeCompletionContext::fileItemsForFolder(KDevelop:
 PythonCodeCompletionContext::PythonCodeCompletionContext(DUContextPointer context, const QString& text, const KDevelop::CursorInRevision& position, int depth): CodeCompletionContext(context, text, position, depth)
 {
     kDebug() << text;
-    QRegExp importfile("(.*)[\\s]*import[\\s]$");
+    
+    QRegExp importsub("(.*)\n[\\s]*from(.*)import[\\s]*$");
+    importsub.setMinimal(true);
+    bool is_importSub = importsub.exactMatch(text);
+    if ( is_importSub ) {
+        QStringList for_module_match = importsub.capturedTexts();
+        QString for_module = for_module_match.last().replace(" ", "");
+        kDebug() << for_module_match;
+        m_operation = PythonCodeCompletionContext::ImportSubCompletion;
+        m_subForModule = for_module;
+        return;
+    }
+    
+    QRegExp importfile("(.*)\n[\\s]*import[\\s]*$");
     importfile.setMinimal(true);
-    QRegExp memberaccess("");
     bool is_importfile = importfile.exactMatch(text);
-    if ( is_importfile ) m_operation = PythonCodeCompletionContext::ImportFileCompletion;
+    QRegExp fromimport("(.*)\n[\\s]*from[\\s]*$");
+    fromimport.setMinimal(true);
+    bool is_fromimport = fromimport.exactMatch(text);
+    if ( is_importfile || is_fromimport ) {
+        m_operation = PythonCodeCompletionContext::ImportFileCompletion;
+        return;
+    }
+    
+    QRegExp memberaccess("");
     kDebug() << "Is import file: " << is_importfile;
 //     Q_ASSERT(false);
 }
--- codecompletion/pythoncodecompletioncontext.h
+++ codecompletion/pythoncodecompletioncontext.h
@@ -22,18 +22,21 @@ public:
     enum CompletionContextType {
         ImportFileCompletion,
         MemberAccessCompletion,
-        DefaultCompletion
+        DefaultCompletion,
+        ImportSubCompletion
     };
     
     PythonCodeCompletionContext(DUContextPointer context, const QString& text, const KDevelop::CursorInRevision& position, int depth);
     virtual QList< KDevelop::CompletionTreeItemPointer > completionItems(bool& abort, bool fullCompletion = true);
     QList<ImportFileItem*> includeFileItems();
     QList<ImportFileItem*> fileItemsForFolder(KDevelop::ProjectFolderItem* folder, KDevelop::IProject* project);
+    QList<ImportFileItem*> findFilesForName(const QString& name);
     
     CompletionContextType m_operation;
     QStack<ProjectFolderItem*> m_folderStack;
     int m_maxFolderScanDepth;
-    QString m_searchingForModule;
+    QStringList m_searchingForModule;
+    QString m_subForModule;
 };
 
 }

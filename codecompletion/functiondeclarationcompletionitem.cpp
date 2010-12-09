d682757053b6ba69f32dc52b0264a079ae481737
Sven Brauch <svenbrauch@googlemail.com>
2010-12-09 15:58:49 +0100

    More autocompletion

--- codecompletion/functiondeclarationcompletionitem.cpp
+++ codecompletion/functiondeclarationcompletionitem.cpp
@@ -2,12 +2,15 @@
 #include <language/codecompletion/normaldeclarationcompletionitem.h>
 #include <language/duchain/declaration.h>
 #include <language/duchain/functiondeclaration.h>
-#include <ktexteditor/document.h>
+
+#include <KTextEditor/View>
+#include <KTextEditor/Document>
 
 #include "functiondeclarationcompletionitem.h"
 #include "navigation/navigationwidget.h"
 
 using namespace KDevelop;
+using namespace KTextEditor;
 
 namespace Python {
 
@@ -20,7 +23,14 @@ void FunctionDeclarationCompletionItem::executed(KTextEditor::Document* document
     Q_ASSERT(decl.data());
     kDebug() << "declaration data: " << decl.data();
     const QString suffix = "()";
+    int skip = 2; // place cursor behind bracktes
+    if ( decl.data()->defaultParametersSize() != 0 ) {
+        skip = 1; // place cursor in brackets if there's parameters
+    }
     document->replaceText(word, decl.data()->identifier().toString() + suffix);
+    if ( View* view = document->activeView() ) {
+        view->setCursorPosition( Cursor(word.end().line(), word.end().column() + skip) );
+    }
 }
 
 FunctionDeclarationCompletionItem::~FunctionDeclarationCompletionItem() { }

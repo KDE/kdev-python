cd57bf6b70cbc76fe97d492a20dff49efd287960
Sven Brauch <svenbrauch@googlemail.com>
2011-02-06 15:07:07 +0100

    Added missing file

---
+++ documentation/test.py
@@ -0,0 +1,12 @@
+class Exception:
+	pass
+
+def type():pass
+def getattr():pass
+def len():pass
+def hasattr():pass
+def int():pass
+def str():pass
+def super():pass
+def list():pass
+def dir():pass

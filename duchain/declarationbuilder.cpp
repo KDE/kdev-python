0efa76c64f1b96b5ae9aa58c36113e1802e78428
Sven Brauch <svenbrauch@googlemail.com>
2012-11-07 16:33:55 +0100

    fix crash when internal functions file is not yet parsed

--- duchain/declarationbuilder.cpp
+++ duchain/declarationbuilder.cpp
@@ -1695,13 +1695,15 @@ void DeclarationBuilder::visitArguments( ArgumentsAst* node )
                                                                   <AbstractType>("string", currentContext());
                 VariableLengthContainer::Ptr dictType = ExpressionVisitor::typeObjectForIntegralType
                                                                   <VariableLengthContainer>("dict", currentContext());
-                lock.unlock();
-                dictType->addKeyType(stringType);
-                node->kwarg->startCol = node->arg_col_offset; node->kwarg->endCol = node->arg_col_offset + node->kwarg->value.length() - 1;
-                node->kwarg->startLine = node->arg_lineno; node->kwarg->endLine = node->arg_lineno;
-                visitVariableDeclaration<Declaration>(node->kwarg, 0, dictType.cast<AbstractType>());
-                workingOnDeclaration->setHasKwarg(true);
-                type->addArgument(dictType.cast<AbstractType>());
+                if ( dictType && stringType ) {
+                    lock.unlock();
+                    dictType->addKeyType(stringType);
+                    node->kwarg->startCol = node->arg_col_offset; node->kwarg->endCol = node->arg_col_offset + node->kwarg->value.length() - 1;
+                    node->kwarg->startLine = node->arg_lineno; node->kwarg->endLine = node->arg_lineno;
+                    visitVariableDeclaration<Declaration>(node->kwarg, 0, dictType.cast<AbstractType>());
+                    workingOnDeclaration->setHasKwarg(true);
+                    type->addArgument(dictType.cast<AbstractType>());
+                }
             }
         }
     }

7d869bda2d84b21ac78d4121d916fc74d3e3e6ab
Sven Brauch <svenbrauch@googlemail.com>
2012-12-05 19:53:30 +0100

    Fix a build error with newer kdevelop

--- debugger/debugjob.cpp
+++ debugger/debugjob.cpp
@@ -38,7 +38,6 @@ void DebugJob::start()
     
     setStandardToolView(KDevelop::IOutputView::DebugView);
     setBehaviours(KDevelop::IOutputView::Behaviours(KDevelop::IOutputView::AllowUserClose) | KDevelop::IOutputView::AutoScroll);
-    setModel(new KDevelop::OutputModel());
     setTitle(m_interpreter + m_scriptUrl.path());
     
     startOutput();

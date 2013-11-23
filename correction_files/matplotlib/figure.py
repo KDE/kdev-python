b635c904cf3726759852f930dab0946f06489518
Sven Brauch <svenbrauch@googlemail.com>
2013-11-23 17:01:46 +0100

    add matplotlib correction files as an example

---
+++ correction_files/matplotlib/figure.py
@@ -0,0 +1,12 @@
+from matplotlib import axes
+
+class class_AxesStack:
+    def function_add(self):
+        l_a = axes.Axes()
+
+class class_Figure:
+    def function_add_axes(self):
+        l_a = axes.Axes()
+
+    def function_gca(self):
+        returns = axes.Axes()
---
+++ correction_files/matplotlib/pyplot.py
@@ -0,0 +1,5 @@
+from matplotlib import axes
+from matplotlib.backend_bases import FigureManagerBase
+
+def function_subplots():
+    l_axarr = [axes.Axes()]

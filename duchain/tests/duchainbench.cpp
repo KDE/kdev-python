b52062620fbd49c445a1463bf4b149528984383f
Sven Brauch <svenbrauch@googlemail.com>
2014-03-31 11:56:02 +0200

    make execution time for benchmarks shorter

--- duchain/tests/duchainbench.cpp
+++ duchain/tests/duchainbench.cpp
@@ -78,7 +78,7 @@ ReferencedTopDUContext DUChainBench::parse(const QString& code)
     TestFile* testfile = new TestFile(code + "\n", "py", 0, testDir.absolutePath().append("/"));
     createdFiles << testfile;
     testfile->parse((TopDUContext::Features) (TopDUContext::ForceUpdate | TopDUContext::AST) );
-    testfile->waitForParsed(500);
+    testfile->waitForParsed(100);
 
     if ( testfile->isReady() ) {
         m_ast = static_cast<Python::ParseSession*>(testfile->topContext()->ast().data())->ast;
@@ -111,20 +111,20 @@ void DUChainBench::benchSimpleStatements_data()
     QTest::addColumn<QString>("code");
 
     // test assignment
-    QTest::newRow("test_nondistinct_assignment_repeated") << QString("a=3\n").repeated(1000);
-    QTest::newRow("test_nondistinct_assignment_looped") << repeat_distinct(QString("a=%X\n"), 1000);
-    QTest::newRow("test_distinct_assignment_repeated") << repeat_distinct(QString("a%X=3\n"), 1000);
-    QTest::newRow("test_distinct_assignment_looped") << repeat_distinct(QString("a%X=%X\n"), 1000);
+    QTest::newRow("test_nondistinct_assignment_repeated") << QString("a=3\n").repeated(200);
+    QTest::newRow("test_nondistinct_assignment_looped") << repeat_distinct(QString("a=%X\n"), 200);
+    QTest::newRow("test_distinct_assignment_repeated") << repeat_distinct(QString("a%X=3\n"), 200);
+    QTest::newRow("test_distinct_assignment_looped") << repeat_distinct(QString("a%X=%X\n"), 200);
     // test function
-    QTest::newRow("test_bare_function") << repeat_distinct(QString("def main%X():\n    pass\n"), 500);
-    QTest::newRow("test_return_function") << repeat_distinct(QString("def main%X():\n    return %X\n"), 500);
+    QTest::newRow("test_bare_function") << repeat_distinct(QString("def main%X():\n    pass\n"), 100);
+    QTest::newRow("test_return_function") << repeat_distinct(QString("def main%X():\n    return %X\n"), 100);
     QTest::newRow("test_arg_function_return_var") << repeat_distinct(QString("def func%X(arg):\n    return arg\na%X = func%X(3)\n"), 200);
     QTest::newRow("test_arg_function_return_fixed") << repeat_distinct(QString("def func%X(arg):\n    return 3\na%X = func%X()\n"), 200);
     // test if statements
-    QTest::newRow("test_if_statement") << repeat_distinct(QString("if(True):\n    pass\n"), 1000);
-    QTest::newRow("test_if_else_statement") << repeat_distinct(QString("if(True):\n    pass\nelse:\n    pass\n"), 1000);
+    QTest::newRow("test_if_statement") << repeat_distinct(QString("if(True):\n    pass\n"), 200);
+    QTest::newRow("test_if_else_statement") << repeat_distinct(QString("if(True):\n    pass\nelse:\n    pass\n"), 200);
     // test for loops
-    QTest::newRow("test_for_loop") << repeat_distinct(QString("for i in range(20):\n    pass\n"), 500);
+    QTest::newRow("test_for_loop") << repeat_distinct(QString("for i in range(20):\n    pass\n"), 100);
     QTest::newRow("test_for_loop_enum") << repeat_distinct(QString("for key, value in enumerate({1:2, 7:3}):\n    pass\n"), 200);
     QTest::newRow("test_for_loop_list") << repeat_distinct(QString("for key, value in [(3, 5), (7, 9)]:\n    pass\n"), 200);
 }

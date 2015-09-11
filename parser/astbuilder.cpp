086642115e0eea23a7dfc836ca0d6febf980242f
Sven Brauch <mail@svenbrauch.de>
2015-09-11 14:26:10 +0200

    fix a few more corner cases of what was broken by python 3.4.3

--- parser/astbuilder.cpp
+++ parser/astbuilder.cpp
@@ -157,12 +157,9 @@ public:
         QChar previous = ' ';
         QChar c = ' ';
 
-        auto last = line.left(offset).trimmed();
-        auto lastChar = last.isEmpty() ? QChar() : last.at(last.size()-1);
-        if ( start->astType != Ast::NameAstType && opening.contains(lastChar) ) {
-            // when not a name, the start is not inside the block, so we skip the first closing parenthesis
-            blocks.push(closing.at(opening.indexOf(lastChar)));
-        }
+        auto inString = [&blocks]() {
+            return ! blocks.isEmpty() && (blocks.top() == '"' || blocks.top() == '\"');
+        };
 
         // while the expression lasts, step through it char by char to find attribute access tokens
         while ( offset < line.size() ) {
@@ -170,7 +167,7 @@ public:
                 previous = c;
             }
             c = line.at(offset);
-            qDebug() << c << blocks;
+//             qDebug() << c << blocks;
 //             qDebug() << c << atDot << dots << inRightBlock << blocks.size() << node->startCol << offset;
             // continue to next line if applicable
             // TODO escaping
@@ -191,7 +188,7 @@ public:
             if ( ! blocks.isEmpty() && blocks.top() == c ) {
                 blocks.pop();
             }
-            else if ( opening.contains(c) ) {
+            else if ( ! inString() && opening.contains(c) ) {
                 blocks.push(closing.at(opening.indexOf(c)));
             }
             else if ( blocks.isEmpty() && closing.contains(c) && ! dots.isEmpty() ) {

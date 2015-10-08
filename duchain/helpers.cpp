8c517ec96aa99c4823a2f9229423eaa9f39ae865
Sven Brauch <mail@svenbrauch.de>
2015-10-08 17:57:30 +0200

    adjust fallback paths for python 3.5

--- duchain/helpers.cpp
+++ duchain/helpers.cpp
@@ -429,8 +429,8 @@ QList<QUrl> Helper::getSearchPaths(const QUrl& workingOnDocument)
         }
         else {
             qCWarning(KDEV_PYTHON_DUCHAIN) << "Could not get search paths! Defaulting to stupid stuff.";
-            searchPaths.append(QUrl::fromLocalFile("/usr/lib/python2.7"));
-            searchPaths.append(QUrl::fromLocalFile("/usr/lib/python2.7/site-packages"));
+            searchPaths.append(QUrl::fromLocalFile("/usr/lib/python3.5"));
+            searchPaths.append(QUrl::fromLocalFile("/usr/lib/python3.5/site-packages"));
             QString path = qgetenv("PYTHONPATH");
             QStringList paths = path.split(':');
             foreach ( const QString& path, paths ) {

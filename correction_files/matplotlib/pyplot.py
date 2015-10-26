089a7b1f17f6400fb35f7033abf82cfb1b895bc2
Sven Brauch <mail@svenbrauch.de>
2015-10-26 23:51:48 +0100

    matplotlib: correctly document return value of plt.subplots()
    worth doing by hand, very common entry-point for type inference

--- correction_files/matplotlib/pyplot.py
+++ correction_files/matplotlib/pyplot.py
@@ -1,7 +1,8 @@
+import matplotlib.pyplot as plt
+import matplotlib.axes._axes
 from matplotlib.figure import *
 from matplotlib.axes import *
 def function_figure():
     returns = Figure()
 def function_subplots():
-    l_ret = (Figure(), Axes())
-    l_axarr = [Axes()]
+    l_ret = (plt.Figure(), matplotlib.axes._axes.Axes() if None else [matplotlib.axes._axes.Axes()] if None else [[matplotlib.axes._axes.Axes()]])

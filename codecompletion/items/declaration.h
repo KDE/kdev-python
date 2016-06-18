da0bb6a9bc507f42e0a3a07f67311e1196974575
Sven Brauch <mail@svenbrauch.de>
2016-06-18 13:33:23 +0200

    completion: implement shortenedTypeString()

--- codecompletion/items/declaration.h
+++ codecompletion/items/declaration.h
@@ -1,16 +1,16 @@
 /*****************************************************************************
- * Copyright (c) 2011 Sven Brauch <svenbrauch@googlemail.com>                *
+ * Copyright (c) 2011-2016 Sven Brauch <svenbrauch@googlemail.com>           *
  *                                                                           *
  * This program is free software; you can redistribute it and/or             *
  * modify it under the terms of the GNU General Public License as            *
  * published by the Free Software Foundation; either version 2 of            *
  * the License, or (at your option) any later version.                       *
- *                                                                           *           
+ *                                                                           *
  * This program is distributed in the hope that it will be useful,           *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
  * GNU General Public License for more details.                              *
- *                                                                           *   
+ *                                                                           *
  * You should have received a copy of the GNU General Public License         *
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
  *****************************************************************************
@@ -31,9 +31,12 @@ public:
     PythonDeclarationCompletionItem(KDevelop::DeclarationPointer decl = KDevelop::DeclarationPointer(), 
                                     QExplicitlySharedDataPointer<KDevelop::CodeCompletionContext> context = QExplicitlySharedDataPointer<KDevelop::CodeCompletionContext>(), 
                                     int inheritanceDepth = 0);
-    virtual QVariant data(const QModelIndex& index, int role, const KDevelop::CodeCompletionModel* model) const;
+    virtual QVariant data(const QModelIndex& index, int role, const KDevelop::CodeCompletionModel* model) const override;
+    QString shortenedTypeString(KDevelop::DeclarationPointer decl, int desiredTypeLength) const override;
+
     void setTypeHint(PythonCodeCompletionContext::ItemTypeHint type);
     void addMatchQuality(int add);
+
 protected:
     PythonCodeCompletionContext::ItemTypeHint m_typeHint;
     int m_addMatchQuality;

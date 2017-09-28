f0e5d2e47dcd6145d88c49751821ca8c8a5a0c9e
Francis Herne <mail@flherne.uk>
2017-09-28 17:31:17 +0100

    Avoid dereferencing null pointer.
    Fixes ca1051be

--- duchain/usebuilder.cpp
+++ duchain/usebuilder.cpp
@@ -61,7 +61,7 @@ DUContext* UseBuilder::contextAtOrCurrent(const CursorInRevision& pos)
 }
 
 void UseBuilder::useHiddenMethod(ExpressionAst* value, Declaration* function) {
-    if ( function->topContext() == Helper::getDocumentationFileContext() ) {
+    if ( !function || function->topContext() == Helper::getDocumentationFileContext() ) {
         // Don't add a use for e.g. `list.__getitem__` in `foo[0]`, no-one cares.
         return;
     }

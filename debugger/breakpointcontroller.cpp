1aa882cab857f1e7f50a14cd97891d76ea13baaf
Jarmo Tiitto <jarmo.tiitto@gmail.com>
2024-04-18 03:12:27 +0300

    kdevpdb: BreakpointController implementation
    Implement insertion and removal of breakpoints:
    * Inserting and erasing a breakpoint in PausedState and ActiveState now
      works. If a breakpoint is inserted/erased in ActiveState, the
      commands to do so are processed immediately with the help of
      flushCommands().
    * Updating the breakpoint's location now works, so breakpoints can be
      added during the debug session.
    * Breakpoint enable/disable state is not yet implemented.
    
    The BreakpointController now maintains a QSharedPointer of
    BreakpointData for each model breakpoint in a std::vector.
    
    The addBreakpoint() queues 'break' commands only on session startup, and
    afterwards this code path is untaken. Just created model breakpoints
    don't have a valid location, so after the startup phase, further
    breakpoint insertions are handled solely by the updateBreakpoint() as
    location updates.
    
    If a queued 'break' command fails: The rejected breakpoint id is reset,
    the m_debuggerBreakpointId is decremented, and all greater than the
    failed ids are decremented so that the breakpoint id reservations will
    match the server state.
    
    Breakpoint commands have to be conditionally scheduled in the
    updateBreakpoint() or removeBreakpoint() methods since many (possibly
    conflicting or would fail) operations can be currently on-going for a
    breakpoint. Therefore, the decisions that depend on the breakpointId or
    the modelBreakpoint are made in a defer handler, where all of the
    earlier operations have finished, and so the BreakpointData is
    up-to-date.
    
    In general, the deferred handlers must check the modelBreakpoint isn't
    a nullptr, because removeBreakpoint() call can take place before the
    queued handlers actually run. As an example, removeBreakpoint() can
    prevent a 'break' command from being queued in the updateHandler().
    There exists other such interactions between the response handlers.
    
    v2: use defer() API
    v3: adapted code to pass command arguments correctly for the changed
        request() API.

--- debugger/breakpointcontroller.cpp
+++ debugger/breakpointcontroller.cpp
@@ -1,5 +1,6 @@
 /*
     SPDX-FileCopyrightText: 2012 Sven Brauch <svenbrauch@googlemail.com>
+    SPDX-FileCopyrightText: 2024 Jarmo Tiitto <jarmo.tiitto@gmail.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
@@ -10,9 +11,14 @@
 #include <QDebug>
 #include "debuggerdebug.h"
 
+#include <algorithm>
+
+// TODO: BreakpointController is not yet invoking notifyHit() to tell the model a breakpoint was hit...
+
 namespace Python {
-    
-BreakpointController::BreakpointController(IDebugSession* parent): IBreakpointController(parent)
+
+BreakpointController::BreakpointController(IDebugSession* parent)
+    : IBreakpointController(parent)
 {
     qCDebug(KDEV_PYTHON_DEBUGGER) << "constructing breakpoint controller";
     connect(debugSession(), SIGNAL(event(IDebugSession::event_t)), this, SLOT(slotEvent(IDebugSession::event_t)));
@@ -25,45 +31,244 @@ DebugSession* BreakpointController::session()
 
 void BreakpointController::slotEvent(IDebugSession::event_t evt)
 {
-    if ( evt == IDebugSession::connected_to_program ) {
-        const auto breakpoints = breakpointModel()->breakpoints();
+    const auto breakpoints = breakpointModel()->breakpoints();
+
+    if (evt == IDebugSession::debugger_exited) {
+        // Reset breakpoint states.
         for (Breakpoint* const bp : breakpoints) {
-            // TODO
+            bp->setState(Breakpoint::NotStartedState);
         }
+        return;
+    }
+    if (evt != IDebugSession::connected_to_program) {
+        return;
+    }
+
+    qCDebug(KDEV_PYTHON_DEBUGGER) << "insert" << breakpoints.size() << "model breakpoints";
+
+    for (Breakpoint* const bp : breakpoints) {
+        addBreakpoint(bp);
     }
 }
 
 void BreakpointController::breakpointAdded(int row)
 {
     auto* modelBreakpoint = breakpointModel()->breakpoint(row);
-
-    qCDebug(KDEV_PYTHON_DEBUGGER) << "adding breakpoint: " << modelBreakpoint << "( line:" << modelBreakpoint->line()
-                                  << ")";
-    // TODO
-
-    // A test.
-    session()->debugger()->defer([](const ResponseData&) {
-        qCDebug(KDEV_PYTHON_DEBUGGER) << "Testing 1st";
-    });
-    session()->debugger()->defer([](const ResponseData&) {
-        qCDebug(KDEV_PYTHON_DEBUGGER) << "Testing 2nd.";
-    });
-    session()->flushCommands();
+    addBreakpoint(modelBreakpoint);
 }
 
 void BreakpointController::breakpointModelChanged(int row, BreakpointModel::ColumnFlags columns)
 {
-    // TODO
-    Q_UNUSED(row);
-    Q_UNUSED(columns);
+    columns &= BreakpointModel::LocationColumnFlag;
+    if (!columns) {
+        return;
+    }
+
+    auto* const modelBreakpoint = breakpointModel()->breakpoint(row);
+    updateBreakpoint(modelBreakpoint);
+
+    // TODO: - implement breakpoint enable/disable state.
+    //       - implement breakpoint hit count. (feedback on breakpoint hit)
+    //       - implement breakpoint ignore hits.
+    //       - implement breakpoint condition.
 }
 
 void BreakpointController::breakpointAboutToBeDeleted(int row)
 {
     auto* modelBreakpoint = breakpointModel()->breakpoint(row);
-    qCDebug(KDEV_PYTHON_DEBUGGER) << "removing breakpoint: " << modelBreakpoint << "( line:" << modelBreakpoint->line()
-                                  << ")";
-    // TODO
+    removeBreakpoint(modelBreakpoint);
+}
+
+bool BreakpointController::isSupportedBreakpoint(Breakpoint* bp)
+{
+    static const QRegularExpression pyfiles(QStringLiteral(".*\\.py.*$"), QRegularExpression::CaseInsensitiveOption);
+
+    if (bp->url().isEmpty() || bp->savedLine() == -1) {
+        return false;
+    } else if (!pyfiles.match(bp->url().path()).hasMatch()) {
+        return false;
+    } else if (bp->kind() != KDevelop::Breakpoint::CodeBreakpoint || !bp->expression().isEmpty()) {
+        // No support for expressions... of any kind.
+        qCDebug(KDEV_PYTHON_DEBUGGER) << "Implement me: kdevpdb has no support for expression breakpoints";
+        return false;
+    }
+
+    return true;
+}
+
+std::pair<QString, int> BreakpointController::locationForBreakpoint(Breakpoint* bp)
+{
+    /*
+     * Breakpoint::savedLine() is used for the location since it should match the document's state on disk.
+     * This also makes the debugger ignore unsaved modifications of the document, which allows the
+     * user to minimally *modify* the document while debugging. (E.g. comment the code)
+     */
+    return std::make_pair(bp->url().path(), bp->savedLine() + 1);
+}
+
+std::vector<BreakpointDataPtr>::iterator BreakpointController::lookupBreakpointData(Breakpoint* bp)
+{
+    return std::find_if(m_breakpoints.begin(), m_breakpoints.end(), [bp](const BreakpointDataPtr& brk) -> bool {
+        return brk->modelBreakpoint == bp;
+    });
+}
+
+void BreakpointController::addBreakpoint(Breakpoint* bp)
+{
+    // We have not added this breakpoint yet, right?
+    if (lookupBreakpointData(bp) != m_breakpoints.end()) {
+        qCWarning(KDEV_PYTHON_DEBUGGER) << "model breakpoint" << bp << "already registered";
+        return;
+    }
+
+    // Add entry for the model breakpoint.
+    const auto& brk = m_breakpoints.emplace_back(BreakpointDataPtr::create(bp));
+    const auto location = locationForBreakpoint(bp);
+
+    if (!isSupportedBreakpoint(bp)) {
+        // updateBreakpoint() inserts the breakpoint once it changes to something valid.
+        qCDebug(KDEV_PYTHON_DEBUGGER) << "breakpoint " << bp << "location" << location.first << ':' << location.second
+                                      << "rejected";
+        bp->setState(Breakpoint::DirtyState);
+        return;
+    }
+
+    qCDebug(KDEV_PYTHON_DEBUGGER) << "inserting breakpoint" << bp << location.first << ':' << location.second;
+
+    bp->setState(Breakpoint::PendingState);
+
+    // (try) insert the PDB breakpoint.
+    brk->breakpointId = m_debuggerBreakpointId++;
+    session()->debugger()->request({CMD_BREAK, location.first, location.second}, [this, brk](const ResponseData& d) {
+        addHandler(d, brk);
+    });
+    session()->flushCommands();
+}
+
+void BreakpointController::addHandler(const ResponseData& data, const BreakpointDataPtr& brk)
+{
+    if (responseValue(data, QStringLiteral("error")).isUndefined()) {
+        // Insertion succeeded.
+        if (brk->modelBreakpoint) {
+            brk->modelBreakpoint->setState(Breakpoint::CleanState);
+        }
+        return;
+    }
+
+    if (brk->modelBreakpoint) {
+        brk->modelBreakpoint->setState(Breakpoint::DirtyState);
+    }
+
+    qCDebug(KDEV_PYTHON_DEBUGGER) << "breakpoint insertion failed:" << brk->breakpointId.value();
+
+    // Pdb rejected the breakpoint's location, and therefore the server didn't use the brk->breakpointId.
+    --m_debuggerBreakpointId;
+    const int invalidId = brk->breakpointId.value();
+    // Cancel a possible already queued removeHandler()
+    brk->breakpointId.reset();
+    // Subtract one from all PDB ids greater than invalidId to correct them.
+    for (const auto& breakpoint : m_breakpoints) {
+        if (breakpoint->breakpointId.value_or(-1) < invalidId) {
+            continue;
+        }
+        qCDebug(KDEV_PYTHON_DEBUGGER) << "fix-up pending id:" << breakpoint->breakpointId.value() << "as"
+                                      << breakpoint->breakpointId.value() - 1;
+        breakpoint->breakpointId.value()--;
+    }
+}
+
+void BreakpointController::removeBreakpoint(Breakpoint* bp)
+{
+    auto itr = lookupBreakpointData(bp);
+    if (itr == m_breakpoints.end()) {
+        // No breakpoint inserted for bp currently?
+        qCWarning(KDEV_PYTHON_DEBUGGER) << "no BreakpointData found for" << bp;
+        return;
+    }
+
+    // Erase from m_breakpoints. (swap-and-pop-back)
+    BreakpointDataPtr brk(std::move(*itr));
+    itr->swap(m_breakpoints.back());
+    m_breakpoints.pop_back();
+    // Cancel all on-going insertions of this model breakpoint by updateHandler().
+    brk->modelBreakpoint = nullptr;
+
+    session()->debugger()->defer([this, brk](const ResponseData&) {
+        if (removeHandler(brk)) {
+            session()->flushCommands();
+        }
+    });
+    session()->flushCommands();
+}
+
+bool BreakpointController::removeHandler(const BreakpointDataPtr& brk)
+{
+    if (!brk->breakpointId) {
+        // No PDB breakpoint available to clear.
+        return false;
+    }
+    int delId = brk->breakpointId.value();
+    brk->breakpointId.reset();
+    session()->debugger()->request({CMD_CLEAR, delId});
+    return true;
+}
+
+void BreakpointController::updateBreakpoint(Breakpoint* bp)
+{
+    auto itr = lookupBreakpointData(bp);
+    if (itr == m_breakpoints.end()) {
+        // No breakpoint inserted for bp currently?
+        qCWarning(KDEV_PYTHON_DEBUGGER) << "no BreakpointData found for" << bp;
+        return;
+    }
+
+    BreakpointDataPtr brk = *itr;
+    const auto location = locationForBreakpoint(bp);
+
+    if (!isSupportedBreakpoint(bp)) {
+        // Rejected.
+        qCDebug(KDEV_PYTHON_DEBUGGER) << "updated breakpoint " << bp << "location" << location << "rejected";
+        bp->setState(Breakpoint::DirtyState);
+
+        session()->debugger()->defer([this, brk](const ResponseData&) {
+            if (removeHandler(brk)) {
+                session()->flushCommands();
+            }
+        });
+        session()->flushCommands();
+        return;
+    }
+
+    qCDebug(KDEV_PYTHON_DEBUGGER) << "updated breakpoint " << bp << "location" << location << "accepted";
+
+    bp->setState(Breakpoint::PendingState);
+
+    // Queue a update of the breakpoint.
+    session()->debugger()->defer([this, brk, location](const ResponseData&) {
+        updateHandler(brk, location);
+    });
+    session()->flushCommands();
+}
+
+void BreakpointController::updateHandler(const BreakpointDataPtr& brk, std::pair<QString, int> location)
+{
+    // TODO: Apply breakpoint enable/disabled state, and don't do re-insert if the location didn't change.
+
+    // Only way to change the location is to create a new PDB breakpoint,
+    // so erase the old PDB breakpoint first, if it exist.
+    bool flush = removeHandler(brk);
+    // Insert only if the model breakpoint has not been deleted yet.
+    if (brk->modelBreakpoint) {
+        brk->breakpointId = m_debuggerBreakpointId++;
+        session()->debugger()->request({CMD_BREAK, location.first, location.second},
+        [this, brk](const ResponseData& d) {
+            addHandler(d, brk);
+        });
+        flush = true;
+    }
+    if (flush) {
+        session()->flushCommands();
+    }
 }
 }
 
--- debugger/breakpointcontroller.h
+++ debugger/breakpointcontroller.h
@@ -1,5 +1,6 @@
 /*
     SPDX-FileCopyrightText: 2012 Sven Brauch <svenbrauch@googlemail.com>
+    SPDX-FileCopyrightText: 2024 Jarmo Tiitto <jarmo.tiitto@gmail.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
@@ -7,12 +8,36 @@
 #ifndef BREAKPOINTCONTROLLER_H
 #define BREAKPOINTCONTROLLER_H
 
+#include <QSharedPointer>
+#include <QString>
+
 #include <debugger/interfaces/ibreakpointcontroller.h>
 #include "debugsession.h"
+#include "pdbdebuggerinstance.h"
+
+#include <vector>
+#include <optional>
 
 using namespace KDevelop;
 
 namespace Python {
+
+struct BreakpointData
+{
+    /// PDB breakpoint id. No value, if no breakpoint is inserted in PDB.
+    std::optional<int> breakpointId;
+    /// The model breakpoint associated with the PDB breakpoint.
+    KDevelop::Breakpoint* modelBreakpoint = nullptr;
+
+    Q_DISABLE_COPY_MOVE(BreakpointData)
+
+    BreakpointData(KDevelop::Breakpoint* bp)
+        : modelBreakpoint(bp)
+    {
+    }
+};
+
+using BreakpointDataPtr = QSharedPointer<BreakpointData>;
 
 class BreakpointController : public KDevelop::IBreakpointController
 {
@@ -26,6 +51,17 @@ public:
 
     void breakpointAboutToBeDeleted(int row) override;
 
+    /**
+     * Note: this method exists to ease the API transition; it should be removed at once
+     *       when this method is no longer called by KDevelop. We don't want KDevelop's
+     *       debuggerStateChanged() default implementation to override the breakpoint states that
+     *       we have setup.
+     **/
+    void debuggerStateChanged(IDebugSession::DebuggerState state) override
+    {
+        Q_UNUSED(state);
+    }
+
 public Q_SLOTS:
     /**
      * @brief Handles events in the debug session.
@@ -35,6 +71,7 @@ public Q_SLOTS:
      * @param evt passed by kdevplatform, specifies the type of the event.
      **/
     void slotEvent(IDebugSession::event_t evt);
+
 protected:
     /**
      * Note: this method exists solely to full fill KDevelop::IBreakpointController
@@ -46,6 +83,52 @@ protected:
         Q_UNUSED(breakpoint);
     }
     DebugSession* session();
+
+private:
+    /// Registered model breakpoints, unordered.
+    std::vector<BreakpointDataPtr> m_breakpoints;
+    /*
+     * What PDB breakpoint Id would be created next. PDB breakpoint ids start from 1 and are never
+     * reused in the same session. Must be incremented *before* queuing a CMD_BREAK.
+     */
+    int m_debuggerBreakpointId = 1;
+
+    static inline const auto CMD_BREAK = QStringLiteral("break");
+    static inline const auto CMD_CLEAR = QStringLiteral("clear");
+
+    static bool isSupportedBreakpoint(Breakpoint* bp);
+    static std::pair<QString, int> locationForBreakpoint(Breakpoint* bp);
+
+    /**
+     * Find the BreakpointData for a model breakpoint.
+     */
+    std::vector<BreakpointDataPtr>::iterator lookupBreakpointData(Breakpoint* bp);
+
+    /**
+     * Add a given model breakpoint to the debugger.
+     * @param bp The breakpoint to add
+     **/
+    void addBreakpoint(Breakpoint* bp);
+    void addHandler(const ResponseData& data, const BreakpointDataPtr& brk);
+
+    /**
+     * Queue commands to remove a given model breakpoint from the debugger.
+     * @param bp The breakpoint to remove
+     **/
+    void removeBreakpoint(Breakpoint* bp);
+
+    /**
+     * Queue a PDB clear command if possible.
+     * @return true if flushCommands() is required afterwards.
+     */
+    bool removeHandler(const BreakpointDataPtr& brk);
+
+    /**
+     * Queue commands to update a given model breakpoint in the debugger.
+     * @param bp The breakpoint to update
+     **/
+    void updateBreakpoint(Breakpoint* bp);
+    void updateHandler(const BreakpointDataPtr& brk, std::pair<QString, int> location);
 };
 
 }

8db6b05f3d9962e91fa4dcfa1a81a3f663aaa791
Jarmo Tiitto <jarmo.tiitto@gmail.com>
2024-07-06 22:30:48 +0300

    kdevpdb: communicate using kdevPdbConnection
    Replace the in-band communication through stdout with an out-of-band
    system using kdevPdbConnection. The debugger is now nearly silent in
    respect to stdout/stderr; uncaught exceptions are still printed as
    normal. However, the debugger remains completely *broken* until the new
    communication system is also implemented on the debugger plugin side.
    
    On launching kdevpdb.py:
    The command line argument "-d" must be provided for kdevpdb.py to
    specify the debugger script's install directory. The unix socket is
    currently created at /tmp/kdevpdb.socket. The target command line shall
    be specified after '--' separator.
    
    The command input, i.e., a request to the debugger, happens with
    {"seq":<int>, "input":<command-string> } data-frames. The
    <command-string> must be terminated by a '\n' char. The "seq" field in
    the requests and responses enables keeping track of the queued
    requests' responses.
    
    Certain commands cause kdevPdb.cmdloop() to return; therefore, the
    response is only sent after a request has been fully completed just
    before acquiring the next request with getDataFrame().
    
    Now, responses are sent from the debugger as {"seq":<int>, "data":
    [{}]...] }" data-frames. The response "data" value is always a list of
    objects. This allows appending any number of objects into a response.
    Each such response field provides a string key, which is used to tell
    what kind of object it is. Currently, the possible response fields are:
    
    * {"message" : "string"} a unstructured output message string.
      This field is added by self.message() and provides all output which
      is otherwise generated by Pdb.
    * {"error" : "string"} a unstructured error message string.
      This field is added by self.error() and provides all errors which are
      otherwise generated by Pdb.
    
    More of such response fields will be defined in later commits. It is
    preferred to implement our own versions of used commands to structure
    them instead of relying on the unstructured "message" fields from
    the Pdb and Bdb module.
    
    Although the client can fully determine what seqnro is used through the
    request-response round-trip, it shall use a max(last-request-seqnro +
    1, last-response-seqnro + 1) as the new seqnro when sending a request.
    This because the kdevpdb.py will send, on start up, a response which
    has no client sent request associated with it.

--- debugger/kdevpdb.py
+++ debugger/kdevpdb.py
@@ -1,46 +1,180 @@
 # SPDX-FileCopyrightText: 2014 Sven Brauch <svenbrauch@gmail.com>
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-from pdb import *
+import pdb
+import json
+import getopt
+import kdevpdbconn
 
-import sys
 
+class kdevPdb(pdb.Pdb):
+    ''' KDevelop python debugger (kdevpdb)
+        A few points:
+        * kdevPdb normally shouldn't output anything into stdout or stderr, and message()
+          and error() have been overridden below to mostly reach this.
+        * kdevPdb replaces a few of key methods from Pdb to implement the out-of-band
+          communication, and to structure command output into JSON data.
+    '''
+    def __init__(self, kdevpath):
+        # Pin down the required modules as member attributes:
+        self.json = json
+        self.kdevpdbconn = kdevpdbconn
+        self.pdbsrv = kdevpdbconn.kdevPdbConnection()
+        # Immediately adjust sys.path as the first "command".
+        preinit = {"seq": -1, "input": f"import sys; sys.path.append({kdevpath!r})\n"}
+        self.pdbsrv.dataframes.append(self.json.dumps(preinit).encode())
+        # Connect to a client.
+        self.pdbsrv.establish()
+        # The command's output is associated with it by the command sequence number. The
+        # seqnro begins from -1, because of the above preinit request.
+        # * cmdloop() receives the command sequence number "seq" (seqnro) from the client. The
+        #   command output is then appended into that response entry.
+        # * cmdloop() then transfers the response JSON data to the client
+        #   (KDevelop) with the specified seqnro just before acquiring the next command from
+        #   kdevPdbConnection. This allows the client to reliably tell what the executed
+        #   command's response was.
+        self.command_seqnro = -1
+        self.responses = {}
+        # Pdb appends an operation into self.cmdqueue on start up to display the stack-frame.
+        # The response of this is reported with seqnro -1 only after preinit has been processed.
+        self.debugger_initialized = False
+        # Init the super class:
+        # - Disable tab complete
+        super().__init__(None)
+        self.prompt = ""
 
-class kdevOutputFormatter():
-    def __init__(self):
-        from sys import stdout as __stdout
-        self.__stdout = __stdout
-    def write(self, data):
-        self.__stdout.write("__KDEVPYTHON_BEGIN_DEBUGGER_OUTPUT>>>" + str(data) + "<<<__KDEVPYTHON_END___DEBUGGER_OUTPUT")
+    def append_response(self, obj):
+        assert isinstance(obj, dict)
+        self.responses.setdefault(self.command_seqnro, []).append(obj)
 
-class kdevPdb(Pdb):
-    def __init__(self):
-        Pdb.__init__(self)
-        self.stdout = kdevOutputFormatter()
-        self.prompt = "__KDEVPYTHON_DEBUGGER_PROMPT"
-        # hack to make the debugger *not* restart the program in any case
-        self.only_once = False
-    
-    def debug_trace(self, *args):
-        '''Set a tracepoint in the Python debugger that works with Qt'''
+    def send_response(self):
+        '''Send the current response to KDevelop'''
+        resp = self.responses.pop(self.command_seqnro, [])
+        fmt = {"seq": self.command_seqnro, "data": resp}
+        self.pdbsrv.sendDataFrame(self.json.dumps(fmt).encode())
+
+    def message(self, msg):
+        '''Buffer a command output message.
+           JSON: "message" : str()
+        '''
+        self.append_response({"message": msg})
+
+    def error(self, msg):
+        '''Buffer a command error message.
+           JSON: "error" : str()
+        '''
+        self.append_response({"error": msg})
+
+    def postcmd(self, stop, line):
+        return stop
+
+    def preloop(self):
+        pass
+
+    def cmdloop(self, intro=None):
+        """Process the kdevPdbConnection received commands.
+           This method replaces the cmd.Cmd.cmdloop() implementation.
+           TODO: Adapting the code of Pdb.precmd(), cmd.Cmd.onecmd() and cmd.Cmd.parseline() for
+                Kdevelop's purposes:
+                In Pdb.precmd(), kdevPdb does not need recognize any command aliases. We should
+                never have multiple commands per a request either.
+                cmd.Cmd.onecmd() and cmd.Cmd.parseline() would allow to go away with the line
+                based commands, removing the need for '\n', and having the command's arguments
+                fed in as JSON "args" : [] list.
+                Overall, such changes could simplify the overall code needed for processing.
+        """
+        self.preloop()
         try:
-            from PyQt4.QtCore import pyqtRemoveInputHook
-            pyqtRemoveInputHook()
-        except ImportError:
-            pass
-        self.set_trace(sys._getframe().f_back)
-    
-    def _runscript(self, filename):
-        import signal
-        import os
-        self._user_requested_quit = 1
-        if self.only_once:
-            os._exit(0)
-        self.only_once = True
-        signal.signal(signal.SIGINT, self.debug_trace)
-        Pdb._runscript(self, filename)
+            stop = None
+            while not stop:
+                if self.cmdqueue:
+                    line = self.cmdqueue.pop(0)
+                else:
+                    if self.debugger_initialized:
+                        self.send_response()
+                    # Wait for a request.
+                    request = self.json.loads(self.pdbsrv.getDataFrame().decode())
+                    assert "seq" in request
+                    self.command_seqnro = int(request["seq"])
+                    line = request["input"]
+                    assert line.endswith('\n')
+                    if not line:
+                        line = 'EOF'
+                    else:
+                        line = line.rstrip('\r\n')
+                    if self.command_seqnro == -1:
+                        # reached the last (and only) preinit command.
+                        self.debugger_initialized = True
+                line = self.precmd(line)
+                stop = self.onecmd(line)
+                stop = self.postcmd(stop, line)
+            self.postloop()
+        except self.kdevpdbconn.Stop:
+            raise
+        except BaseException as e:
+            print(e)
+
+def main():
+    """Kdevelop python debugger main, based on pdb.main()"""
+    # Pin down pdb.Restart pdb.traceback from pdb, and import sys
+    import sys
+    traceback = pdb.traceback
+    Restart = pdb.Restart
+
+    # Target command line is expected after the first '--'.
+    split = [i for i in range(len(sys.argv)) if sys.argv[i] == '--']
+    if not split or split[0] + 1 >= len(sys.argv):
+        sys.exit(2)
+
+    split = split[0]
+    opts, args = getopt.getopt(sys.argv[1:split], 'd:mc:', ['command='])
+
+    if len(opts) < 1 or args:
+        sys.exit(2)
+
+    commands = [optarg for opt, optarg in opts if opt in ['-c', '--command']]
+    module_indicated = any(opt in ['-m'] for opt, optarg in opts)
+    kdevpath = [optarg for opt, optarg in opts if opt == '-d'][0]
+
+    cls = pdb._ModuleTarget if module_indicated else pdb._ScriptTarget
+    args = sys.argv[split+1:]
+    target = cls(args[0])
+    target.check()
+    sys.argv[:] = args      # Hide "pdb.py" and pdb options from argument list
+    # Note on saving/restoring sys.argv: it's a good idea when sys.argv was
+    # modified by the script being debugged. It's a bad idea when it was
+    # changed by the user from the command line. There is a "restart" command
+    # which allows explicit specification of command line arguments.
+    debugger = kdevPdb(kdevpath)
+    debugger.rcLines.extend(commands)
+
+    exitcode = 0
+    try:
+        # NOTE: _run() *replaces* the __main__ namespace with the target's __main__. This
+        #       means that any objects that we haven't pinned as local names in this routine
+        #       will now cease to exist, since all references to other objects can be dropped.
+        debugger._run(target)
+    except Restart:
+        # kdevpdb does not allow restarting.
+        pass
+    except debugger.kdevpdbconn.Stop:
+        # Client exited
+        pass
+    except SystemExit as e:
+        # In most cases SystemExit does not warrant a post-mortem session.
+        print("The program exited via sys.exit(). Exit status:", end=' ')
+        print(e)
+    except SyntaxError:
+        traceback.print_exc()
+        exitcode = 1
+    except BaseException as e:
+        traceback.print_exc()
+        print("Uncaught exception. Entering post mortem debugging")
+        t = e.__traceback__
+        debugger.interaction(None, t)
+        exitcode = 1
+    debugger.pdbsrv.stop()
+    sys.exit(exitcode)
 
 if __name__ == '__main__':
-    import pdb
-    pdb.Pdb = kdevPdb
-    pdb.main()
+    main()

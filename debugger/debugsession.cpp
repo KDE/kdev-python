91a69fa0e0010c800da692e6339a9a079a9f6123
Jarmo Tiitto <jarmo.tiitto@gmail.com>
2024-08-04 04:49:25 +0300

    kdevpdb: Rewrite of DebugSession core
    This commit re-implements the core of the debugger such that:
    * Most of the user actions for stepping, continuing and like do work.
    * Interrupting a long running command that resumed the inferior works;
      This is an major improvement compared to the previous situation.
    * Proper ActiveState/PausedState switching; The debugger only enters
      ActiveState, when the inferior's code is going to be executed.
    * Frame-stack view works.
    * Execution location update works.
    * Inferior's stderr/stdout is simply forwarded into the console as is.
    * Killing the session/debugger works.
    
    Possible debugger hangs are resolved by launching a timer in the
    stopDebugger() action to ensure the debugger process will be killed
    after 5 seconds. Normally, the process ends fast enough before this, so
    the timer won't expire.
    
    Everything else should be assumed to be broken or missing; No variable
    support is implemented and breakpoints aren't implemented at all.
    Subsequent commits will reintroduce more of the now lost functionality a
    case by case basis.
    
    v3: Even better stepOut() behavior; User can step out of loops and step
    over conditional blocks. If not within a conditional block, steps out of
    the most recent stack-frame.
    
    v4: Adapt to the recently introduced StartupInfo.

--- debugger/debugsession.cpp
+++ debugger/debugsession.cpp
@@ -16,9 +16,9 @@
 #include "debugsession.h"
 #include "breakpointcontroller.h"
 #include "pdbframestackmodel.h"
-#include "startupinfo.h"
 #include "variablecontroller.h"
 #include "variable.h"
+#include "pdbprocess.h"
 
 #include <QDebug>
 #include <QRegularExpression>
@@ -34,11 +34,17 @@ DebugSession::DebugSession()
     , m_breakpointController(nullptr)
     , m_variableController(nullptr)
     , m_frameStackModel(nullptr)
+    , m_resumingFinished([this](const ResponseData& d) {
+        resumingFinished(d);
+    })
 {
     qCDebug(KDEV_PYTHON_DEBUGGER) << "creating debug session";
     m_breakpointController = new Python::BreakpointController(this);
-    m_variableController = new VariableController(this);
     m_frameStackModel = new PdbFrameStackModel(this);
+    m_variableController = new VariableController(this);
+    m_debugger = new PdbDebuggerInstance(this);
+
+    connect(&m_killTimer, &QTimer::timeout, this, &DebugSession::killDebuggerNow);
 }
 
 IBreakpointController* DebugSession::breakpointController() const
@@ -56,9 +62,47 @@ IFrameStackModel* DebugSession::frameStackModel() const
     return m_frameStackModel;
 }
 
+PdbDebuggerInstance* DebugSession::debugger() const
+{
+    // start() must have been invoked once before this method!
+    Q_ASSERT(m_debugger);
+    Q_ASSERT(m_debugger->instance());
+    Q_ASSERT(m_debugger->instance()->process());
+    return m_debugger;
+}
+
 void DebugSession::start(const StartupInfo& info)
 {
-    // TODO
+    m_state = StartingState;
+    // Disable UI controls, so the user cannot try e.g. run() until we are ready.
+    raiseEvent(debugger_busy);
+    Q_EMIT stateChanged(m_state);
+
+    // Connect lower-level PdbProcess signals to our slots:
+    auto* const debugger = m_debugger->instance();
+    connect(debugger, &PdbProcess::ready, this, &DebugSession::debuggerInit);
+    connect(debugger, &PdbProcess::running, this, &DebugSession::debuggerBusy);
+    connect(debugger, &PdbProcess::suspended, this, &DebugSession::inferiorSuspended);
+    connect(debugger, &PdbProcess::finished, this, &DebugSession::finalizeState);
+    connect(debugger, &PdbProcess::stdoutAvailable, this, &DebugSession::stdoutData, Qt::QueuedConnection);
+    connect(debugger, &PdbProcess::stderrAvailable, this, &DebugSession::stderrData, Qt::QueuedConnection);
+
+    // The debugger sends a "frames" response when it has initialized.
+    m_debugger->setMethod(m_debugger->currentSeqnro(), m_resumingFinished);
+
+    // Launch the process.
+    debugger->start(info);
+}
+
+void DebugSession::debuggerInit()
+{
+    // Successfully connected to the debugger.
+    // debuggerBusy() slot was skipped since we haven't actually sent any commands
+    // yet. Thus, set up the states such that inferiorSuspended() brings us into
+    // PausedState, once the initial response is processed after this slot.
+    m_resumingRequest = true;
+    m_state = ActiveState;
+    Q_EMIT stateChanged(m_state);
 }
 
 QStringList byteArrayToStringList(const QByteArray& r) {
@@ -85,104 +129,200 @@ void DebugSession::stderrData(QByteArray data)
     Q_EMIT stderrReceived(byteArrayToStringList(data));
 }
 
-#if 0 // temporally keep this code as place holder for git.
-        raiseEvent(debugger_ready);
-        if ( currentUrl().isValid() ) {
-            Q_EMIT showStepInSource(currentUrl(), currentLine(), currentAddr());
-        }
-#endif
+void DebugSession::debuggerBusy()
+{
+    // Switch to active state if we are running user commands. Such commands are those
+    // that would resume running the inferior's code. Other commands shouldn't switch
+    // between the PausedState or ActiveState.
+    if (!m_resumingRequest || m_state != PausedState) {
+        return;
+    }
+    m_state = ActiveState;
+    qCDebug(KDEV_PYTHON_DEBUGGER) << PausedState << "==>" << m_state;
+    raiseEvent(debugger_busy);
+    raiseEvent(program_running);
+    Q_EMIT stateChanged(m_state);
+}
+
+void DebugSession::inferiorSuspended()
+{
+    if (!m_resumingRequest || m_state != ActiveState) {
+        return;
+    }
+    qCDebug(KDEV_PYTHON_DEBUGGER) << m_state << "==>" << PausedState;
+
+    if (!m_sessionStarted) {
+        m_sessionStarted = true;
+        qCDebug(KDEV_PYTHON_DEBUGGER) << "debugger initialized successfully!";
+        raiseEvent(connected_to_program);
+    }
+
+    m_resumingRequest = false;
+    m_state = PausedState;
+    Q_EMIT stateChanged(m_state);
+    raiseEvent(debugger_ready);
+    // Program state has changed.
+    raiseEvent(program_state_changed);
+}
 
 void DebugSession::stepOut()
 {
-    // TODO this only steps out of functions; use temporary breakpoints for loops maybe?
-    // TODO
+    m_resumingRequest = true;
+    m_debugger->request({QStringLiteral("stepout")}, m_resumingFinished);
 }
 
 void DebugSession::stepOverInstruction()
 {
-    // TODO
+    m_resumingRequest = true;
+    m_debugger->request({QStringLiteral("next")}, m_resumingFinished);
 }
 
 void DebugSession::stepInto()
 {
-    // TODO
+    m_resumingRequest = true;
+    m_debugger->request({QStringLiteral("step")}, m_resumingFinished);
 }
 
 void DebugSession::stepIntoInstruction()
 {
-    // TODO
+    m_resumingRequest = true;
+    m_debugger->request({QStringLiteral("step")}, m_resumingFinished);
 }
 
 void DebugSession::stepOver()
 {
-    // TODO
+    m_resumingRequest = true;
+    m_debugger->request({QStringLiteral("next")}, m_resumingFinished);
 }
 
 void DebugSession::jumpToCursor()
 {
+    m_resumingRequest = true;
     if (KDevelop::IDocument* doc = KDevelop::ICore::self()->documentController()->activeDocument()) {
         KTextEditor::Cursor cursor = doc->cursorPosition();
         if ( cursor.isValid() ) {
-            // TODO disable all other breakpoints
-            // WIP
+            // TODO: consider a specialized handler, since "jump" can refuse to do anything.
+            m_debugger->request({QStringLiteral("jump"), cursor.line() + 1}, m_resumingFinished);
         }
     }
 }
 
 void DebugSession::runToCursor()
 {
+    // Would be a resuming request, but this method can't be implemented yet...
+    // m_resumingRequest = true;
     if (KDevelop::IDocument* doc = KDevelop::ICore::self()->documentController()->activeDocument()) {
         KTextEditor::Cursor cursor = doc->cursorPosition();
         if ( cursor.isValid() ) {
             // TODO disable all other breakpoints
             QString temporaryBreakpointLocation = doc->url().path() + QLatin1Char(':') + QString::number(cursor.line() + 1);
-            // TODO
+            // TODO: need temporary breakpoint support in our BreakpointController...
         }
     }
 }
 
 void DebugSession::run()
 {
-    // TODO
+    // The m_resumingRequest is set here (and this applies to other such stepping commands also)
+    // to trigger entering into ActiveState. debuggerBusy() then disables the debugger UI controls,
+    // and thus prevents the user from trying again until we reach PausedState.
+    m_resumingRequest = true;
+    m_debugger->request({QStringLiteral("continue")}, m_resumingFinished);
 }
 
 void DebugSession::interruptDebugger()
 {
-    // TODO
+    m_debugger->instance()->tryInterrupt();
 }
 
-void DebugSession::updateLocation()
+void DebugSession::resumingFinished(const ResponseData& data)
 {
-    qCDebug(KDEV_PYTHON_DEBUGGER) << "updating location";
-    // TODO
-}
+    // Update execution location.
+    // This handler is invoked by any of the resuming actions, and
+    // the "frames" field contains just the most recent frame. (this response is guaranteed)
 
-void DebugSession::locationUpdateReady(QByteArray data) {
-    // TODO
+    const auto frame = responseArray(data, QStringLiteral("frames")).at(0).toObject();
+    const auto file = frame.value(QStringLiteral("filename")).toString();
+    const int line = frame.value(QStringLiteral("line")).toInt();
+    const int addr = frame.value(QStringLiteral("address")).toInt();
+
+    setCurrentPosition(QUrl::fromLocalFile(file), line - 1, QString::number(addr));
+    qCDebug(KDEV_PYTHON_DEBUGGER) << "New position: " << file << line - 1;
 }
 
 void DebugSession::stopDebugger()
 {
-    // TODO
+    if (m_state >= StoppingState || m_state == NotStartedState) {
+        return;
+    }
+    qCDebug(KDEV_PYTHON_DEBUGGER) << "stopping debugger";
+    m_state = StoppingState;
+    Q_EMIT stateChanged(m_state);
+
+    if (!m_debugger || !m_debugger->instance()) {
+        return;
+    }
+    // Tell the server to quit eventually.
+    m_debugger->instance()->sendControlCommand(PdbProcess::ControlCommand::Terminate);
+    m_debugger->instance()->tryInterrupt();
+
+    // Ensure we won't wait forever for the process to die.
+    m_killTimer.setSingleShot(true);
+    m_killTimer.setInterval(5000);
+    m_killTimer.start();
 }
 
 void DebugSession::killDebuggerNow()
 {
-    // TODO
+    killDebugger();
 }
 
 void DebugSession::finalizeState()
 {
-    // TODO
+    // Disarm the kill timer, the process exited.
+    m_killTimer.stop();
+
+    if (m_state == EndedState) {
+        // finalizeState() already invoked.
+        return;
+    } else if (m_state < StoppingState) {
+        // Normal exit of the inferior. (we skipped the StoppingState.)
+        raiseEvent(program_exited);
+    }
+
+    m_state = EndedState;
+    raiseEvent(debugger_exited);
+    Q_EMIT stateChanged(m_state);
+}
+
+void DebugSession::killDebugger()
+{
+    if (m_state == EndedState || m_state == NotStartedState) {
+        return;
+    }
+    // Kill timer expired or must stop in a hurry.
+    qCDebug(KDEV_PYTHON_DEBUGGER) << "killing debugger now";
+    if (m_state < StoppingState) {
+        m_state = StoppingState;
+        Q_EMIT stateChanged(m_state);
+    }
+
+    if (m_debugger && m_debugger->instance()) {
+        // Be gone!
+        m_debugger->instance()->killNow();
+    }
+
+    finalizeState();
 }
 
 DebugSession::~DebugSession()
 {
-    // TODO
+    killDebugger();
 }
 
 void DebugSession::restartDebugger()
 {
+    // Not implemented. There seems to be no user action for this in KDevelop?
 }
 
 bool DebugSession::restartAvaliable() const

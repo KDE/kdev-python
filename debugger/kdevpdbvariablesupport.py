d95fa5e5878e6bb69ecbf62d2b0c0a83637665b9
Jarmo Tiitto <jarmo.tiitto@gmail.com>
2024-09-29 01:23:50 +0300

    kdevpdb: kdevExprValueMapper backend
    Implement support for enumerating and inspecting variables, replacing
    the obsoleted module with the new kdevpdbvariablesupport.py. The
    subsequent commit provides the implementation for enumerating
    stack-frame locals, which uses the functionality provided by this
    commit.
    
    The kdevPdb debugger now supports specifying "variable namespaces". The
    namespaces are maintained in the kdevExprValueMapper.objectsByNamespace
    dictionary, where each namespace is referred to with an id. Namespaces
    provide a way to isolate a collection of objects, which, if enumerated,
    would otherwise collide by their names.
    
    The kdevExprValueMapper.objectsByHandle dictionary holds all pinned
    values to which the longnames in each namespace can refer. A longname
    is an hierarchical (path-like) name by which the debugger can refer to
    a particular Python object. Each longname can have two handles
    associated with it, one for the most up-to-date value and a second
    handle for the previous value.
    
    The inspectvalue() function can set up an enumerator for a handle while
    also reporting other details about the value to which the handle refers.
    The enumerator is a Python generator that yields all the children of the
    handle in question.
    
    When both previous and current values are available, the changed state
    of the object can also be computed. For this, enumeratevariables()
    tries to shallow copy the enumerated values when possible and maintains
    the pair of handles to make this old/new value comparison possible.
    
    Handles for which the inspectvalue() reported a child count of > 0 can
    be further fed into the enumeratevariables() requests.
    enumeratevariables() can incrementally report more child handles until
    the enumerator is exhausted.
    
    However, namespace handles are only intended to be used with
    enumeratevariables() requests because it's expected that such commands
    also set up the enumerator for the reported namespace handle.
    
    After the program state has changed, a cleanupobjects() request must
    take place to discard all obsoleted states and to allow re-enumerating
    all of the variables again.

---
+++ debugger/kdevpdbvariablesupport.py
@@ -0,0 +1,403 @@
+# SPDX-FileCopyrightText: 2024 Jarmo Tiitto <jarmo.tiitto@gmail.com>
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import types
+import math
+import copy
+# pylint: disable=C0103, R0903, C0115, R1710
+# This module implements the kdevPdb's variable machinery and other helpers.
+
+
+class enumeratorBase():
+    '''Enumerate a list of things.
+       Sub classes must compute the sequence length for super().__init__(...).
+    '''
+    def __init__(self, ref, size):
+        self.objectref = ref
+        self.size = size
+
+    def __len__(self):
+        return self.size
+
+    def next(self):
+        '''Sub classes must override this method,
+           and set self.objectref = None once completed.'''
+        self.objectref = None
+
+
+class namespaceEnumerator(enumeratorBase):
+    '''Enumerate a namespace object.'''
+    def __init__(self, obj):
+        # filter out some uninteresting python internals.
+        filtter = ('__builtins__', '__file__', '__name__', '__spec__')
+        obj = [(x, y) for x, y in obj if x not in filtter]
+        super().__init__(obj, len(obj))
+
+    def next(self):
+        for x, y in self.objectref:
+            assert isinstance(x, str)
+            yield (x, y)
+        self.objectref = None
+
+
+class attributeEnumerator(enumeratorBase):
+    '''Enumerate an object attributes with dir() builtin.'''
+    def __init__(self, obj):
+        # Sort, although KDevelop will sort the generated set differently.
+        # The ordering however does help the user to obtain the attributes in
+        # a much nicer order if all of the attributes aren't immediately enumerated.
+        g = [[], [], [], [], [], []]
+        for x in dir(obj):
+            # order callable attributes after non-callable.
+            iscallable = callable(getattr(obj, x))
+            if len(x) >= 2 and x[:2] == '__':
+                g[4 + iscallable].append(x)
+            elif x[0] == '_':
+                g[2 + iscallable].append(x)
+            else:
+                g[0 + iscallable].append(x)
+        self.attribs = [x for l in g for x in l]
+        hint = len(g[0]) + len(g[1])
+        self.expandhint = min(10, len(self.attribs)) if hint == 0 else hint
+        super().__init__(obj, len(self.attribs))
+
+    def next(self):
+        for x in self.attribs:
+            yield (x, getattr(self.objectref, x))
+        self.objectref = None
+
+
+class listEnumerator(enumeratorBase):
+    '''Enumerate an list() like sequence with enumerate().'''
+    def __init__(self, obj):
+        super().__init__(obj, len(obj))
+
+    def next(self):
+        for i, x in enumerate(self.objectref):
+            yield (f'[{i}]', x)
+        self.objectref = None
+
+
+class dictEnumerator(enumeratorBase):
+    '''Enumerate an dict() like object.'''
+    def __init__(self, obj):
+        super().__init__(obj, len(obj) * 2)
+
+    def next(self):
+        for i, x in enumerate(self.objectref.items()):
+            yield (f'[{i}].key', x[0])
+            yield (f'[{i}].value', x[1])
+        self.objectref = None
+
+
+class chunkEnumerator(enumeratorBase):
+    '''Enumerate a string like object in MAX_LEN length chunks'''
+    MAX_LEN = 100
+
+    def __init__(self, obj):
+        super().__init__(obj, math.ceil(len(obj) / chunkEnumerator.MAX_LEN))
+
+    def next(self):
+        for i in range(0, len(self.objectref), chunkEnumerator.MAX_LEN):
+            chunk = self.objectref[i:i + chunkEnumerator.MAX_LEN]
+            yield (f'[{i}..{i + len(chunk) - 1}]', chunk)
+        self.objectref = None
+
+
+class Namespace():
+    def __init__(self):
+        self.names = {}    # mapping of handles into their longnames in self.handles[]
+        self.handles = {}  # mapping of longname into the current and old handle
+        self.inited = set()  # set of enumerated longnames.
+
+
+# Reduced set of immutable types. (see copy._copy_immutable())
+_skip_shallowcopy = {types.NoneType, int, float, bool, complex, str,
+                     type, range, slice, property,
+                     types.BuiltinFunctionType, types.EllipsisType,
+                     types.NotImplementedType, types.FunctionType, types.CodeType}
+
+
+def _shallowcopy(value):
+    '''Return a shallow copy of a value if possible or identity'''
+    cls = type(value)
+    if cls in _skip_shallowcopy:
+        return value
+    # pylint: disable=W0718
+    # Note: copy.copy() can raise arbitrary exceptions when it fails.
+    try:
+        return copy.copy(value)
+    except Exception:
+        # Not copy-able: add the type to the ignored set of types,
+        # to skip handling the exception again.
+        _skip_shallowcopy.add(cls)
+        return value
+
+
+def inspectSimpleValue(obj, *_):
+    '''Inspect a simple value with no children'''
+    if isinstance(obj, (types.NoneType, int, bool, float)):
+        return {'count': 0, 'data': str(obj)}
+
+
+def inspectMethodOrFunction(obj, *_):
+    '''Inspect a method or function type.'''
+    try:
+        code = obj.__func__.__code__
+        if code:
+            # Instance method
+            return {'count': 0, 'data': f'{obj.__qualname__}'}
+    except AttributeError:
+        pass
+    try:
+        code = obj.__code__
+        if code:
+            # Function
+            return {'count': 0, 'data': f'{obj.__qualname__}'}
+    except AttributeError:
+        pass
+
+
+def inspectClass(obj, gens, handle):
+    '''Inspect a class type.'''
+    try:
+        if obj.__class__ is type:
+            report = inspectAttributes(obj, gens, handle)
+            report['data'] = f'class {obj.__qualname__}'
+            return report
+    except AttributeError:
+        pass
+
+
+def inspectCallable(obj, *_):
+    '''Inspect a callable type.'''
+    if callable(obj):
+        return {'count': 0, 'data': '<callable>'}
+
+
+def inspectModuleType(obj, gens, handle):
+    '''Inspect a module.'''
+    if not isinstance(obj, types.ModuleType):
+        return None
+    report = inspectAttributes(obj, gens, handle)
+    report['data'] = f"module '{obj.__name__}'"
+    return report
+
+
+def inspectDataString(obj, gens, handle):
+    '''Inspect a string like data.'''
+    if not isinstance(obj, (str, bytes, bytearray, memoryview)):
+        return None
+    # For shorter than chunkEnumerator.MAX_LEN chars, report a direct value with 'len'.
+    # Otherwise, report in chunks as the sequence may be *very* long.
+    # (megabytes perhaps even..)
+    report = {'len': len(obj)}
+    if report['len'] > chunkEnumerator.MAX_LEN:
+        g = chunkEnumerator(obj)
+        gens[handle] = g.next()
+        report['count'] = len(g)
+        return report
+    report['count'] = 0
+    if isinstance(obj, (memoryview, bytearray)):
+        # repr() doesn't work well for these types.
+        report['data'] = repr(bytes(obj))
+        return report
+    report['data'] = repr(obj)
+    return report
+
+
+def inspectSequence(obj, gens, handle):
+    '''Inspect a sequence.'''
+    if not isinstance(obj, (list, tuple, set, frozenset)):
+        return None
+    g = listEnumerator(obj)
+    gens[handle] = g.next()
+    return {'count': len(g), 'len': len(obj), 'expandhint': 3}
+
+
+def inspectDict(obj, gens, handle):
+    '''Inspect a dictionary.'''
+    if not isinstance(obj, dict):
+        return None
+    g = dictEnumerator(obj)
+    gens[handle] = g.next()
+    return {'count': len(g), 'len': len(obj), 'expandhint': 2}
+
+
+def inspectAttributes(obj, gens, handle):
+    '''Inspect attributes of obj.'''
+    # This works on all nearly object types, if we have no better way to represent the value.
+    g = attributeEnumerator(obj)
+    gens[handle] = g.next()
+    return {'count': len(g), 'expandhint': g.expandhint}
+
+
+class kdevExprValueMapper():
+    def __init__(self):
+        self.handleCounter = 0
+        # Mapping of handles into the pinned values.
+        self.objectsByHandle = {}
+        # Mapping of handles into generators.
+        self.objectEnumerators = {}
+        # All namespace objects.
+        self.objectsByNamespace = {}
+        # List of inspect() functions.
+        # The order is critical, the first that doesn't return None wins.
+        self.detectors = [
+            inspectSimpleValue,
+            inspectMethodOrFunction,
+            inspectClass,
+            inspectCallable,
+            inspectModuleType,
+            inspectDataString,
+            inspectSequence,
+            inspectDict,
+            inspectAttributes]
+
+    def makeHandle(self):
+        '''Make a variable handle.'''
+        self.handleCounter += 1
+        return self.handleCounter
+
+    def get_topindex(self):
+        raise NotImplementedError("subclass must implement this method.")
+
+    def cleanupobjects(self):
+        '''Cleanup invalidated state after pausing'''
+        self.objectEnumerators.clear()
+        # Coarse purge handles (and longnames) that are now unreachable
+        # from the possible namespaces.
+        reachable = set()
+        unreachable = set()
+        for ns_id, ns in list(self.objectsByNamespace.items()):
+            if -2 <= ns_id <= self.get_topindex():
+                reachable.update(ns.names)
+            else:
+                unreachable.update(ns.names)
+                # Drop the entire namespace object.
+                del self.objectsByNamespace[ns_id]
+        # Drop handles which are not in reachable.
+        unreachable -= reachable
+        num_purged = len(unreachable)
+        for ptr in unreachable:
+            del self.objectsByHandle[ptr]
+        # Purge all longnames which have not been initialized/used.
+        pinned = len(self.objectsByHandle)
+        for ns in self.objectsByNamespace.values():
+            for longname, handles in list(ns.handles.items()):
+                if longname in ns.inited:
+                    continue
+                ns.handles.pop(longname, None)
+                ns.names.pop(handles[0], None)
+                ns.names.pop(handles[1], None)
+                self.objectsByHandle.pop(handles[0], None)
+                self.objectsByHandle.pop(handles[1], None)
+            ns.inited.clear()
+        num_purged += pinned - len(self.objectsByHandle)
+        return {'purged': num_purged, 'pinned': len(self.objectsByHandle)}
+
+    def updateNamespace(self, items, ns_id, ns_label):
+        '''Create a new namespace handle from a list of tuples'''
+        ns = self.objectsByNamespace.setdefault(ns_id, Namespace())
+        if ns_label in ns.handles:
+            handle = ns.handles[ns_label][0]
+        else:
+            handle = self.makeHandle()
+            # Link the handle inside its own namespace.
+            ns.handles[ns_label] = [handle, 0]
+        ns.names[handle] = ns_label
+        ns.inited.add(ns_label)
+        self.objectsByHandle[handle] = (ns_id, True)
+        g = namespaceEnumerator(items)
+        self.objectEnumerators[handle] = g.next()
+        return len(g), handle
+
+    def enumerateHandle(self, handle, ns_id, do_copy=True):
+        '''Enumerate next variable of a handle.'''
+        try:
+            # try generate the next item.
+            objname, value = next(self.objectEnumerators[handle])
+        except StopIteration:
+            # the generator was exhausted, remove it.
+            self.objectEnumerators.pop(handle, None)
+            return None
+        except KeyError:
+            # No generator was set up.
+            # (the client likely made an mistake)
+            return None
+
+        parent = self.objectsByHandle[handle]
+        if len(parent) == 2:
+            # The handle refers to a namespace.
+            ns = self.objectsByNamespace[parent[0]]
+            longname = objname
+        else:
+            # Build the current longname for value
+            assert ns_id in self.objectsByNamespace
+            ns = self.objectsByNamespace[ns_id]
+            # '/' is used to join child name to the parent name rather than '.' because
+            # this makes it possible later to split the longname into its components.
+            longname = ns.names[handle] + '/' + objname
+
+        if longname in ns.inited:
+            # Already initialized this longname.
+            return {'expression': objname, 'ptr': ns.handles[longname][0]}
+
+        handles = ns.handles.get(longname)
+        if handles is None:
+            # Make a new pair handles.
+            # Initially, the other handle will have no pinned value in objectsByHandle.
+            handles = [self.makeHandle(), self.makeHandle()]
+            ns.handles[longname] = handles
+        # Swap the current and previous handles.
+        handles[0], handles[1] = handles[1], handles[0]
+        # Update the handles[0]'s pinned value with a shallow copy of the value
+        # when possible or reference the value as-is.
+        newvalue = _shallowcopy(value) if do_copy else value
+        self.objectsByHandle[handles[0]] = (newvalue,)
+        # Associate the handle[0] with a longname.
+        # Although ns.names[handles[1]] is technically able to refer to a different
+        # longname than ns.names[handles[0]], I have never observed this happening.
+        # Hence the assert:
+        assert handles[1] not in ns.names or ns.names[handles[1]] == longname
+        ns.names[handles[0]] = longname
+        # Mark the longname as initialized.
+        ns.inited.add(longname)
+        return {'expression': objname, 'ptr': handles[0]}
+
+    def inspectvalue(self, handle, ns_id):
+        ''' Determine the variable's value and other details:
+            - The 'count' number of children items, if any, and 'type'.
+              These fields must exist.
+            Optional fields (can be omitted from the response):
+            - 'data' provides the displayed value of the variable.
+            - 'len' is displayed in place of 'data' as "len=...",
+               or as prefix "len=..., " if both are provided.
+            - 'expandhint' is how many children, at minimum, the client should enumerate.
+               If not reported, this is min(1,count)
+            return: {'count': int(), 'type': str(), 'data': str(),
+                     'len': int(), 'expandhint': int()}
+        '''
+        try:
+            obj = self.objectsByHandle[handle][0]
+        except KeyError:
+            # Not found in the table. Respond with "None" to tell the given handle no longer exists.
+            return None
+
+        # Compute the changed state.
+        changed = False
+        if ns_id is not None:
+            ns = self.objectsByNamespace[ns_id]
+            handles = ns.handles[ns.names[handle]]
+            changed = True
+            if handles[1] in self.objectsByHandle:
+                # assert obj is self.objectsByHandle[handles[0]][0]
+                changed = obj != self.objectsByHandle[handles[1]][0]
+
+        for proc in self.detectors:
+            ret = proc(obj, self.objectEnumerators, handle)
+            if ret is not None:
+                ret.setdefault('changed', changed)
+                ret.setdefault('type', obj.__class__.__qualname__)
+                return ret
+        return None
